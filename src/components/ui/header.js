8990a8670d2b75a300a524455f384833c91a6d1b
Mahilson Pereira <mahilson.pereira@acct.global>
2022-03-07 22:48:42 -0300

    added material custom styled

--- src/components/ui/header.js
+++ src/components/ui/header.js
@@ -6,19 +6,28 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import { makeStyles } from '@material-ui/core/styles';
 
 import search from '../../images/search.svg'
 import cart from '../../images/cart.svg'
 import account from '../../images/account-header.svg'
 
+const useStyles = makeStyles(theme => ({
+  coloredIndicator: {
+    backgroundColor: '#eee'
+  }
+}));
+
 export default function Header() {
+  const handle = useStyles();
+
   return (
     <AppBar color='transparent' elevation={0}>
       <Toolbar>
         <Button>
           <Typography variant="h1">VAR X</Typography>
         </Button>
-        <Tabs>
+        <Tabs value={0} classes={{ indicator: handle.coloredIndicator }}>
           <Tab label="Hats" />
           <Tab label="Hoodies" />
           <Tab label="Shirts" />
